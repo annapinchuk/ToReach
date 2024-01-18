d7c6c96d3707fe559690ca1a6b17de6691865985
ZoharSimhon <Zoharsimhon13@gmail.com>
2024-01-18 14:32:19 +0200

    started updating login

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import { styles } from "./styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.logInScreen.container}>
+      <Image
+        style={styles.logInScreen.logo}
+        source={require('./Images/logo.jpg')}
+      />
+      {/* <Text style={styles.logInScreen.header}>ToReach</Text> */}
       <StatusBar style="auto" />
+      <View style={styles.logInScreen.middleBox}>
+        <Text style={styles.logInScreen.inputBox}> Email </Text>
+        <Text style={styles.logInScreen.inputBox}> Password </Text>
+        <Text> Login </Text>
+        <Text> Forgot password? </Text>
+      </View>
     </View>
   );
 }
+{/* styles = StyleSheet.create(); */ }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
