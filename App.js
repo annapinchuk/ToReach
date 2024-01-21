9399c4d1cc5787dfd23977e51a2c2e1d20a4a69b
ZoharSimhon <Zoharsimhon13@gmail.com>
2024-01-21 21:35:22 +0200

    added navbar WIP

--- App.js
+++ App.js
@@ -7,20 +7,24 @@ import LoginScreen from './src/pages/LoginScreen';
 import RegisterBusinessScreen from './src/pages/RegisterBusinessScreen';
 import RegisterClientScreen from './src/pages/RegisterClientScreen';
 
-
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterBusinessScreen" component={RegisterBusinessScreen} />
-        <Stack.Screen name="RegisterClientScreen" component={RegisterClientScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+          <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          header: () => <></>,
+        }} initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterBusinessScreen" component={RegisterBusinessScreen} />
+          <Stack.Screen name="RegisterClientScreen" component={RegisterClientScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      );
 };
 
 export default App;
