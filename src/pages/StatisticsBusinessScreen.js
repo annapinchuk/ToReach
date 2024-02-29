b1801e72f6c67bf4ecbb57bdf12cc1cddf0f0572
annapinchuk <76442355+annapinchuk@users.noreply.github.com>
2024-02-29 17:18:23 +0200

    working Business statistics
    added pie graphs and calendar
    connected to firebase

--- src/pages/StatisticsBusinessScreen.js
+++ src/pages/StatisticsBusinessScreen.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Button, Text, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
+import MonthPicker from 'react-native-month-year-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { StatusBar } from "expo-status-bar";
+import { app, auth, db } from '../firebaseConfig';
+import { collection, doc, getDocs, getDoc, query, where, orderBy } from '@firebase/firestore';
 
+// import moment from 'moment'; 
 
-const MONTHLY_COLORS = ['#FF5733', '#33FF57', '#5733FF', '#FF5733', '#33FF57', '#5733FF']; // Add more as needed
+let MONTHLY_COLORS = []; // Add more as needed
+
+let totalIncomeYear = 0;
+let totalIncomeMonth = 0;
+
 const YEARLY_COLORS = ['#FF5733', '#33FF57', '#5733FF']; // Add more as needed
 
-const monthlyData = [
-  { month: 'January', income: 500 },
-  { month: 'February', income: 800 },
-  { month: 'March', income: 1200 },
-  // Add more months and incomes as needed
-];
+let monthStatsData = [];
 
-const yearlyData = [
-  { name: 'Appointment Type 1', income: 3000 },
-  { name: 'Appointment Type 2', income: 5000 },
-  { name: 'Appointment Type 3', income: 2000 },
-  // Add more appointment types and incomes as needed
-];
+let yearlyData = [];
 
 const chartConfig = {
   backgroundGradientFrom: '#f8f8f8',
@@ -28,42 +28,218 @@ const chartConfig = {
 };
 
 const StatisticsScreen = () => {
+
+  // ================================== Orel ==========================
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
+  const [show, setShow] = useState(false);
+  const [mode, setMode] = useState('date');
+
+
+  const currentYear = new Date().getFullYear();
+  const minYear = currentYear - 1;
+  const maxYear = currentYear + 1;
+
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    // setShow(false);
+    setDate(currentDate);
+    adjustGraph(currentDate);
+  };
+
+  const showDatePicker = () => {
+    setShow(true);
+  };
+
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+  
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    const exists = MONTHLY_COLORS.find(item => item === color);
+
+    if (exists) {
+      return getRandomColor();
+    }
+    else {
+      MONTHLY_COLORS.push(color);
+    }
+  
+    return color;
+  };
+
+
+  const areDatesInSameMonth = (date1, date2) => {
+    return (
+      date1.getMonth() === date2.getMonth()
+    );
+  };
+
+  const areDatesInSameYear = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear()
+    );
+  };
+
+  const adjustGraph = async (currentDate) => {
+
+    try {
+      // bzero stats
+      monthStatsData = [];
+      yearlyData = [];
+      totalIncomeYear = 0;
+      totalIncomeMonth = 0;
+
+      // Retrieve snapshot
+      const { uid } = auth.currentUser;
+      const appointmentsCollection = collection(db, 'Appointments');
+      const appointmentsQuery = query(
+          appointmentsCollection,
+          where('businessID', '==', uid),
+      );
+      
+      // Get appointments from Firestore
+      const appointmentsSnapshot = await getDocs(appointmentsQuery);
+
+      const appointmentsData = appointmentsSnapshot.docs.map(
+        (doc) => ({
+          businessID: doc.data.businessID,
+          clientID: doc.data.clientID,
+          endTime: doc.data().endTime,
+          name: doc.data().name,
+          price: doc.data().price,
+          startTime: doc.data().startTime,
+        })
+      );
+
+      appointmentsData.forEach((appointment) => {
+        console.log("Name:", appointment.name);
+        let date1 = appointment.endTime.toDate();
+        let date2 =  currentDate;
+
+        // if (showMonthlyChart) {
+          if (areDatesInSameYear(date1, date2)) {
+
+            totalIncomeYear += appointment.price;
+            const nameIndex2 = yearlyData.findIndex(item => item.name === appointment.name);
+
+            if (nameIndex2 !== -1) {
+              yearlyData[nameIndex2].income += appointment.price;
+
+              // stats[appointment.name] += appointment.price;
+            }
+            else {
+              yearlyData.push({name: appointment.name, income: appointment.price})
+              // stats[appointment.name] = appointment.price;
+            }
+
+            if (areDatesInSameMonth(date1, date2)) {
+              totalIncomeMonth += appointment.price;
+              // totalIncome += appointment.price;
+  
+              const nameIndex = monthStatsData.findIndex(item => item.name === appointment.name);
+  
+              if (nameIndex !== -1) {
+                monthStatsData[nameIndex].income += appointment.price;
+  
+                // stats[appointment.name] += appointment.price;
+              }
+              else {
+                monthStatsData.push({name: appointment.name, income: appointment.price})
+                // stats[appointment.name] = appointment.price;
+              }
+            }
+          }
+      });
+      console.log("\n\nsum:", totalIncomeYear, "\n-------------------\n");
+      console.log("\n\nsum:", totalIncomeMonth, "\n-------------------\n");
+      console.log("\n\nmonthStatsData:", monthStatsData, "\n-------------------\n");
+      console.log("\n\nyearlyData:", yearlyData, "\n-------------------\n");
+      console.log("\n\nid:", uid, "\n-------------------\n");
+
+      // monthlyData = stats;
+    }
+    catch (err) {
+      console.log(err);
+    }
+
+  }
+
+  // ================================== Orel ==========================
+  const [pokemon,setPokemon] = useState();
+
+
   const [showMonthlyChart, setShowMonthlyChart] = useState(true);
 
-  const data = showMonthlyChart ? monthlyData : yearlyData;
-  const totalIncome = data.reduce((sum, item) => sum + item.income, 0);
+  let data = showMonthlyChart ? monthStatsData : yearlyData;
+  // const totalIncome = data.reduce((sum, item) => sum + item.income, 0);
+
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Financial Summary</Text>
 
+    <View style={styles.rowContainer}>
+
+      <DateTimePicker
+        value={date}
+        mode={"date"}
+        is24Hour={true}
+        onChange={onChange}
+      />
+    
       <TouchableOpacity
         style={[styles.toggleButton, { backgroundColor: showMonthlyChart ? '#4CAF50' : '#2196F3' }]}
-        onPress={() => setShowMonthlyChart(!showMonthlyChart)}
+        onPress={() => {
+          setShowMonthlyChart(!showMonthlyChart);
+        }}
       >
         <Text style={styles.toggleButtonText}>
-          {showMonthlyChart ? 'Switch to Yearly Chart' : 'Switch to Monthly Chart'}
+          {'Apply Changes'}
         </Text>
       </TouchableOpacity>
+    </View>
 
-      <View style={styles.amountContainer}>
-        <Text style={styles.amountText}>₪10,478.16</Text>
-      </View>
 
       {/* Display expense rows */}
       {/* Example: */}
       <View style={styles.expenseRow}>
-        <Text style={styles.expenseCategory}>עסקאות חוזרות</Text>
-        <Text style={styles.expenseAmount}>₪3,818.83</Text>
+        <Text style={styles.expenseCategory}>מחזור חודשי</Text>
+        <Text style={styles.expenseAmount}>₪{totalIncomeMonth}</Text>
       </View>
       {/* Add more rows as needed */}
 
       {/* Display the colorful pie chart */}
       <PieChart
-        data={data.map((item, index) => ({
-          name: showMonthlyChart ? item.month : item.name,
+        data={monthStatsData.map((item, index) => ({
+          name: item.name,
           income: item.income,
-          color: showMonthlyChart ? MONTHLY_COLORS[index] : YEARLY_COLORS[index],
+          // color: showMonthlyChart ? MONTHLY_COLORS[index] : YEARLY_COLORS[index],
+          color: getRandomColor()
+        }))}
+        width={350}
+        height={200}
+        chartConfig={chartConfig}
+        accessor="income"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
+
+      <View style={styles.expenseRow}>
+        <Text style={styles.expenseCategory}>מחזור שנתי</Text>
+        <Text style={styles.expenseAmount}>₪{totalIncomeYear}</Text>
+      </View>
+
+      <PieChart
+        data={yearlyData.map((item, index) => ({
+          name: item.name,
+          income: item.income,
+          // color: showMonthlyChart ? MONTHLY_COLORS[index] : YEARLY_COLORS[index],
+          color: getRandomColor()
         }))}
         width={350}
         height={200}
@@ -82,6 +258,12 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#ffffff',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: '',
+    marginBottom: 16,
   },
   title: {
     fontSize: 24,
